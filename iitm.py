820f70bb3863d93d9a9c2ceffa04a3a3e28602a4
Mugilan Deiveegan <73863531+mugilanD98@users.noreply.github.com>
2022-01-31 12:56:00 +0530

    Add files via upload

---
+++ iitm.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import lxml
+import schedule
+import time
+
+
+# In[14]:
+
+
+def scrc():
+    html_text=requests.get('https://www.renewablesindia.in/').text
+    soup= BeautifulSoup(html_text,'lxml')
+    location=soup.find('h1',class_='hand').text
+    location=location.strip()
+    renewable=soup.find_all('div',class_='card-heading-1')
+    renew_india=[]
+    for i in range(0,3):
+      renew_india.append(renewable[i].text)
+    renew_india.append(location)
+    print(renew_india)
+
+
+# In[23]:
+
+
+schedule.every(2).minutes.do(scrc)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+# In[ ]:
+
+
+
+
